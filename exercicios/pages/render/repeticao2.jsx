2da99a321e1f5fba849a9fc56ad376db95bffd63
Heittor Oliveira <heittor.oliveira@fornecedores.sicoob.com.br>
2021-08-11 08:20:58 -0300

    criacao arquivo repeticao2

---
+++ exercicios/pages/render/repeticao2.jsx
@@ -0,0 +1,37 @@
+import listaProdutos from '../../data/listaProdutos'
+
+
+export default function repeticao2() {
+
+    function renderizarLinhas() {
+        return listaProdutos.map(produto => {
+            return (
+                <tr key={produto.id}>
+                    <td>{produto.id}</td>
+                    <td>{produto.nome}</td>
+                    <td>{produto.preco}</td>
+                </tr>
+            )
+        })
+    }
+
+    return (
+        <div>
+            <table border="1" style={{
+                border:"1px solid #000"
+            }}>
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Nome</th>
+                        <th>Preço</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {renderizarLinhas()}
+                </tbody>
+            </table>
+
+        </div>
+    )
+};
